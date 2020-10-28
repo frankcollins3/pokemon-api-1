a757f212f2cf978a4f82112542b3de7f181dbec7
Rome Bell <romebell5@gmail.com>
2020-10-28 08:35:55 -0700

    [github] Add new object and element to webpage

--- github.js
+++ github.js
@@ -1,9 +1,22 @@
-fetch('https://api.github.com/users/pbmfkdsa')
+const container = document.querySelector('.container');
+
+fetch('https://api.github.com/users/romebell')
 .then(response => {
     return response.json();
 })
 .then(githubData => {
-    console.log(githubData);
+    const romeObj = {
+        bio: githubData.bio,
+        username: githubData.login,
+        name: githubData.name
+    }
+    console.log(romeObj);
+    
+    const newElement = document.createElement('p');
+    newElement.textContent = romeObj.name;
+    // append newElement to container
+    container.appendChild(newElement);
+
 })
 .catch(error => {
     console.log(error);
